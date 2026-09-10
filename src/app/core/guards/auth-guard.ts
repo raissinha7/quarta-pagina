29284d2fd7ab753ded8ba15d422f14ec8e065d3a
kaylannecunha1902-source <kaylannecunha1902@gmail.com>
2026-09-10 16:35:23 -0300

    fiz o auth guard

--- src/app/core/guards/auth-guard.ts
+++ src/app/core/guards/auth-guard.ts
@@ -1,5 +1,15 @@
-import { CanActivateFn } from '@angular/router';
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { Auth } from '../services/auth';
 
-export const authGuard: CanActivateFn = (route, state) => {
-  return true;
+export const authGuard: CanActivateFn = () => {
+
+  const auth = inject(Auth);
+  const router = inject(Router);
+
+  if (auth.estaLogado()) {
+    return true;
+  }
+
+  return router.createUrlTree(['/login']);
 };
